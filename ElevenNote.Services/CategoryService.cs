f514e6ec8e0ed3d6048e297084ad6798205c52bf
Martin Flaherty <flaherty.martin@gmail.com>
2021-04-21 22:43:32 -0400

    5.01

---
+++ ElevenNote.Services/CategoryService.cs
@@ -0,0 +1,96 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class CategoryService
+    {
+        public bool CreateCategory(CategoryCreate model)
+        {
+            var entity =
+                new Category()
+                {
+                    CategoryId = model.CategoryId,
+                    CategoryName = model.CategoryName
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categories.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CategoryListItem> GetCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Categories
+                        .Select(
+                            e =>
+                                new CategoryListItem
+                                {
+                                    CategoryId = e.CategoryId,
+                                    CategoryName = e.CategoryName
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+        public CategoryDetail GetCategoryById(int categoryId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(e => e.CategoryId == categoryId);
+                return
+                    new CategoryDetail
+                    {
+                        CategoryId = entity.CategoryId,
+                        CategoryName = entity.CategoryName
+                    };
+            }
+        }
+
+        public bool UpdateCategory(CategoryEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(e => e.CategoryId == model.CategoryId);
+
+                entity.CategoryName = model.CategoryName;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteCategory(int categoryId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(e => e.CategoryId == categoryId);
+
+                ctx.Categories.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
